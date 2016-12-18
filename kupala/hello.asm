239405b0554d1dedf2269f4512a0f29392d05a9b
lex archbang pc <lex@archbang>
2016-12-18 20:17:10 +0300

    #6 - Subroutine to Print Strings

--- kupala/hello.asm
+++ kupala/hello.asm
@@ -1,16 +1,36 @@
 section .data
-	text db "hey", 10
+	text db "hey", 10, 0
+	text2 db "hey?", 10, 0
 
 section .text
 	global _start
 
 _start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text
-	mov rdx, 14
-	syscall
+	mov rax, text
+	call _print
+
+	mov rax, text2
+	call _print
 
 	mov rax, 60
 	mov rdi, 0
 	syscall
+; input: rax as pointer to string
+; output: print string at rax
+_print:
+	push rax
+	mov rbx, 0
+_printLoop:
+	inc rax
+	inc rbx
+	mov cl, [rax]
+	cmp cl, 0
+	jne _printLoop
+
+	mov rax, 1
+	mov rdi, 1
+	pop rsi
+	mov rdx, rbx
+	syscall
+
+	ret
